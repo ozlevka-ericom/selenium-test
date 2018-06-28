091d65360716f2b9e71e5160d7dfb0a24e060cd3
ozlevka-ericom <lev.ozeryansky@ericom.com>
2018-06-28 10:15:13 +0300

    Run download container

--- run_test.sh
+++ run_test.sh
@@ -1,4 +1,4 @@
 #!/bin/bash
 
 
-docker run --rm -e "SYSTEM_UNDER_TEST_IP=192.168.50.114" -e "ES_HOST=192.168.50.150:9201" -e "TEST_CYCLES=2" ozlevka/python-test:170907-15.25
+docker run --rm -e "SYSTEM_UNDER_TEST_IP=192.168.50.74" -e "ES_HOST=192.168.50.54:9201" -e "TEST_CYCLES=2" -v /home/ozlevka/projects/selenium-test/download:/opt/driver/logs ozlevka/python-test-download:latest
