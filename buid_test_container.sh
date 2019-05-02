ac10b9a914b4d00790596eefdae5d9dfcbce13e1
ozlevka-ericom <lev.ozeryansky@ericom.com>
2019-05-02 11:44:34 +0300

    Change script to be more flexible Add valid kibana import to data dir

--- buid_test_container.sh
+++ buid_test_container.sh
@@ -1,16 +1,22 @@
-#!/bin/bash -x
+#!/bin/bash
 
-if [ -z "$1" ]; then
+if [[ -z "$1" || "$1" == "upload"   ]]; then
     TAG=`date +"%y%m%d-%H.%M"`
 else
     TAG=$1
 fi
 
-docker build -t ozlevka/python-test:latest .
-docker tag ozlevka/python-test:latest ozlevka/python-test:$TAG
-docker build -t ozlevka/python-test-download:latest -f Dockerfile-download .
-docker tag ozlevka/python-test-download:latest ozlevka/python-test-download:$TAG
+echo "$TAG"
 
-echo "Upload builded scripts"
-docker push ozlevka/python-test:$TAG
-docker push ozlevka/python-test-download:$TAG
+docker build -t ozlevka/python-test:latest .
+docker build -t ozlevka/python-test-download:latest -f Dockerfile-download .
+
+
+if [[ "$1" = "upload" ]]; then
+
+    echo "Upload builded scripts"
+    docker tag ozlevka/python-test:latest ozlevka/python-test:$TAG
+    docker push ozlevka/python-test:$TAG
+    docker tag ozlevka/python-test-download:latest ozlevka/python-test-download:$TAG
+    docker push ozlevka/python-test-download:$TAG
+fi
